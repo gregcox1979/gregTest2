72b33c4a0d30168d0c655c7bcb30b2dfbb355c95
Chanikul Dechpholkrang <chanikul@hotmail.com>
2018-07-01 15:21:47 +0100

    Initial Commit

---
+++ gregTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gregTest2
+//
+//  Created by Gregory Cox on 01/07/2018.
+//  Copyright © 2018 Gregory Cox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
